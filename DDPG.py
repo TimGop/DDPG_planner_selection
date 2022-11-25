changed some hyperparams and fixed softmax incorrect dims
Loss seems to decrease but reward decreases???

DDPG.py
@@ -35,8 +35,8 @@ class DDPG(object):
         self.critic_target.eval()  # removes dropout etc. for evaluation purposes
         self.actor_target.eval()  # removes dropout etc. for evaluation purposes
 
-        self.actor_optimizer = Adam(self.actor.parameters())  # optimizer for actor net
-        self.critic_optimizer = Adam(self.critic.parameters())  # optimizer for critic net
+        self.actor_optimizer = Adam(self.actor.parameters(), lr=0.01)  # optimizer for actor net
+        self.critic_optimizer = Adam(self.critic.parameters(), lr=0.01)  # optimizer for critic net
 
         hard_update(self.critic_target, self.critic)  # make sure _ and target have same weights
         hard_update(self.actor_target, self.actor)  # make sure _ and target have same weights
@@ -58,6 +58,9 @@ class DDPG(object):
         self.set_train()
         state_batch = torch.cat(transition_batch.state).to(device)
         action_batch = torch.stack(transition_batch.action).to(device)
+        # print(state_batch.shape)
+        # print(action_batch.shape)
+        # assert False
         action_batch = action_batch.squeeze(dim=1)
         reward_batch = torch.stack(transition_batch.reward).to(device)
         done_batch = torch.cat(transition_batch.done).to(device)
@@ -78,6 +81,7 @@ class DDPG(object):
         # print("Critic expected values: ", expected_values)
         # print("Critic values: ", state_action_batch)
         value_loss = F.mse_loss(state_action_batch, expected_values.detach())
+        print("val_loss:", value_loss)
         # print("state_action_batch: ", state_action_batch)
         # print("expected_values: ", expected_values.detach())
         # print("val_loss vec: ", expected_values.detach()-state_action_batch)

DDPG_evaluation.py
@@ -29,8 +29,9 @@ def evaluateNetwork(episodeNumbers, averageRewards, currentEpisodeNumber, agent,
 
     for task_i_idx in range(num_of_tests):
         obs, _ = env.reset_testMode(task_i_idx)
-        task_img = torch.from_numpy(obs)
-        state = resize(task_img).unsqueeze(0)
+        # task_img = torch.from_numpy(obs)
+        state = obs  # resize(task_img).unsqueeze(0)
+        state = torch.tensor([state], dtype=torch.float32)
         episode_end = False
         i = 0
         while not episode_end:

DDPG_nets.py
@@ -10,20 +10,22 @@ class Actor(nn.Module):
     def __init__(self, h, w, outputs):
         super(Actor, self).__init__()
         numOutputChannelsConvLayer = 128
-        self.conv2d = nn.Conv2d(1, numOutputChannelsConvLayer, kernel_size=(2, 2), stride=(1, 1))
-        self.batchNormalisation = nn.BatchNorm2d(128)
-        self.dropout = nn.Dropout2d(p=0.5)
-        self.flatten = nn.Flatten()
-        linear_input_size = ((h - 1) * (w - 1) * numOutputChannelsConvLayer)
-        self.hidden = nn.Linear(linear_input_size, 20)
+        # self.conv2d = nn.Conv2d(1, numOutputChannelsConvLayer, kernel_size=(2, 2), stride=(1, 1))
+        # self.batchNormalisation = nn.BatchNorm2d(128)
+        # self.dropout = nn.Dropout2d(p=0.5)
+        # self.flatten = nn.Flatten()
+        self.in_layer = nn.Linear(1, 100)
+        # linear_input_size = ((h - 1) * (w - 1) * numOutputChannelsConvLayer)
+        self.hidden = nn.Linear(100, 20)
         self.headPlanner = nn.Linear(20, outputs)
 
     def forward(self, f_state):
         x = f_state
         x.to(device)
-        x = torch.softmax(
+        x = x.view((-1, 1))
+        x = (torch.softmax(
             self.headPlanner(torch.relu(
-                self.hidden(self.flatten(self.dropout(self.batchNormalisation(torch.relu(self.conv2d(x)))))))), dim=1)
+                self.hidden((torch.relu(self.in_layer(x)))))), dim=1))
         return x
 
 
@@ -31,17 +33,19 @@ class Critic(nn.Module):
     def __init__(self, h, w, outputs=1, NumAdditionalArgsLinLayer=17):
         super(Critic, self).__init__()
         numOutputChannelsConvLayer = 128
-        self.conv2d = nn.Conv2d(1, numOutputChannelsConvLayer, kernel_size=(2, 2), stride=(1, 1))
-        self.batchNormalisation = nn.BatchNorm2d(128)
-        self.dropout = nn.Dropout2d(p=0.5)
-        self.flatten = nn.Flatten()
-        linear_input_size = ((h - 1) * (w - 1) * numOutputChannelsConvLayer) + NumAdditionalArgsLinLayer
-        self.hidden = nn.Linear(linear_input_size, 20)  # numoutputs --> single value
+        # self.conv2d = nn.Conv2d(1, numOutputChannelsConvLayer, kernel_size=(2, 2), stride=(1, 1))
+        # self.batchNormalisation = nn.BatchNorm2d(128)
+        # self.dropout = nn.Dropout2d(p=0.5)
+        # self.flatten = nn.Flatten()
+        # linear_input_size = ((h - 1) * (w - 1) * numOutputChannelsConvLayer) + NumAdditionalArgsLinLayer
+        self.in_layer = nn.Linear(18, 100)
+        self.hidden = nn.Linear(100, 20)  # numoutputs --> single value
         self.headQ = nn.Linear(20, outputs)  # numoutputs --> single value
 
     def forward(self, f_state, action):
         x = f_state
         x.to(device)
-        x = self.flatten(self.dropout(self.batchNormalisation(torch.relu(self.conv2d(x)))))
+        x = x.view((32, 1))
+        # x = self.flatten(self.dropout(self.batchNormalisation(torch.relu(self.conv2d(x)))))
         x_Final_Layer = torch.cat((x, action), dim=1)
-        return self.headQ(torch.relu(self.hidden(x_Final_Layer)))
+        return self.headQ(torch.relu(self.hidden(torch.relu(self.in_layer(x_Final_Layer)))))

DDPG_training.py
@@ -22,7 +22,7 @@ parser.add_argument("--num_episodes", default=3000, type=int,
                     help="Num. of total timesteps of training (default: 3000)")
 parser.add_argument("--gamma", default=0.99,
                     help="Discount factor (default: 0.99)")
-parser.add_argument("--tau", default=0.001,
+parser.add_argument("--tau", default=0.05,
                     help="Update factor for the soft update of the target networks (default: 0.001)")
 parser.add_argument("--EVALUATE", default=10, type=int,
                     help="Number of episodes between testing cycles(default: 10)")
@@ -37,7 +37,7 @@ parser.add_argument("--Theta", default=10, type=int,
 parser.add_argument("--Epsilon", default=1, type=int,
                     help="A constant used for reward calculation(default: 1)")
 args = parser.parse_args()
-#  time_per_ep=1800, ominicron=10, Theta=10, Epsilon=1
+
 # Initialize memory, environment and agent
 memory = ReplayMemory(10000)
 env = portfolio_environment.PortfolioEnvironment(trainingSet, taskFolderLoc, reward, time_per_ep=args.time_per_ep,
@@ -56,9 +56,9 @@ for i_episode in range(args.num_episodes):
     print("\nepisode " + str(i_episode))
     # obs is a dict
     obs, _ = env.reset()
-    task_img = torch.from_numpy(obs)
-    img = resize(task_img)
-    state = img.unsqueeze(0)
+    # task_img = torch.from_numpy(obs)
+    # img = resize(task_img)
+    state = torch.tensor([obs], dtype=torch.float32)  # img.unsqueeze(0)
     final_state = False
     # limit to 1 attempt per episode
     for i in range(1):

portfolio_environment.py
@@ -36,6 +36,8 @@ class PortfolioEnvironment(gym.Env):
         task_name = self.filenames.iloc[self.task_idx]
         self.task_img = np.ascontiguousarray(read_image(os.path.join(
             self.image_dir, task_name + '-bolded-cs.png')), dtype=np.float32) / 255
+        # return self.task_img, {}
+        self.task_img = 1
         return self.task_img, {}
 
     def reset_testMode(self, idx):
@@ -44,6 +46,8 @@ class PortfolioEnvironment(gym.Env):
         task_name = self.filenames.iloc[self.task_idx]
         self.task_img = np.ascontiguousarray(read_image(os.path.join(
             self.image_dir, task_name + '-bolded-cs.png')), dtype=np.float32) / 255
+        # return self.task_img, {}
+        self.task_img = 1
         return self.task_img, {}
 
     def step(self, action):
@@ -53,11 +57,11 @@ class PortfolioEnvironment(gym.Env):
                                            ominicron=self.omnicron, Theta=self.Theta, Epsilon=self.Epsilon)
         if done:
             self.task_img = None
-        return self.task_img, rewardVal, done, True, {}
+        return 1, rewardVal, done, True, {}
 
     @staticmethod
     def get_planner_noise():
-        return torch.normal(torch.zeros((1, 17)), torch.full((1, 17), 0.7))
+        return torch.normal(torch.zeros((1, 17)), torch.full((1, 17), 1.0))
 
     def render(self, mode='human', close=False):
         print(f"Tas: {self.task_idx}")