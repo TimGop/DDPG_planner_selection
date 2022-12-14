7697cdf1e106cbeb040063df6b10f5fc52c07abc
TimGop <71383406+TimGop@users.noreply.github.com>
2022-12-14 22:55:35 +0100

    version of MARL with ddpg reward as MSE

--- DDPG_reward.py
+++ DDPG_reward.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -8,34 +10,15 @@ def reward(taskIndex, plannerCurrNo, plannerCurrTime_unclipped, plannerCurrPrevC
     plannerCurrTime = plannerCurrTime_unclipped if plannerCurrTime_unclipped < time_left_episode else time_left_episode
     minTimeReq_Planners = np.array(df.iloc[taskIndex][1:])
     minTimeReq_Planners[plannerCurrNo] = minTimeReq_Planners[plannerCurrNo] - plannerCurrPrevConsecutiveTime
-    min_val = np.min(minTimeReq_Planners)
-    lower_bound = min_val * 1.1
-    if minTimeReq_Planners[plannerCurrNo] <= time_left_episode:
-        if minTimeReq_Planners[plannerCurrNo] == min_val:
-            upper_bound = min_val * 1.2
-        else:
-            upper_bound = minTimeReq_Planners[plannerCurrNo] * 1.2
+    planner_times_sorted = sorted(minTimeReq_Planners)
+    planner_indices_sorted = sorted(range(len(minTimeReq_Planners)), key=lambda k: minTimeReq_Planners[k])
+    max_pos_still_solved = last_index_solved(planner_times_sorted, time_left_episode)
+    if (max_pos_still_solved + 1) % 2 == 0:
+        median = (planner_times_sorted[math.floor(max_pos_still_solved / 2)] + planner_times_sorted[
+            math.floor((max_pos_still_solved / 2) + 1)]) / 2
     else:
-        upper_bound = lower_bound + 100  # iff cant solve with current planner still reward good time allocation
-    if upper_bound - lower_bound <= 50:
-        upper_bound = lower_bound + 50
-    if upper_bound > time_left_episode:
-        upper_bound = time_left_episode-0.1
-        # upper bound cant be exactly t_left or overestimating will give good reward
-
-    # calculate the reward
-    if plannerCurrTime <= 0:
-        R = -10
-    elif min_val < plannerCurrTime <= lower_bound:
-        R = ((plannerCurrTime - min_val)/(lower_bound - min_val))
-    elif lower_bound < plannerCurrTime <= upper_bound:
-        R = 1
-    elif upper_bound < plannerCurrTime < time_left_episode:
-        R = (plannerCurrTime-time_left_episode)/(upper_bound-time_left_episode)
-    elif plannerCurrTime == time_left_episode:
-        R = 0
-    else:
-        R = 0
+        median = planner_times_sorted[math.floor(max_pos_still_solved / 2)]
+    R = -((plannerCurrTime - median) ** 2)
 
     # check if done
     done = minTimeReq_Planners[plannerCurrNo] <= plannerCurrTime <= time_left_episode
@@ -43,5 +26,6 @@ def reward(taskIndex, plannerCurrNo, plannerCurrTime_unclipped, plannerCurrPrevC
     return np.array([R]), done
 
 
-class Reward:
-    pass
+def last_index_solved(arr, time_left_ep):
+    next_val = next((x for x in arr if x > time_left_ep), None)
+    return 16 if next_val is None else arr.index(next_val) - 1
---
+++ run0.sh
@@ -0,0 +1,14 @@
+#!/bin/bash                 
+#The previous line is mandatory
+ 
+#SBATCH --job-name=myrun     #Name of your job
+#SBATCH --partition=infai_1
+#SBATCH --cpus-per-task=8    #Number of cores to reserve
+#SBATCH --mem-per-cpu=4G     #Amount of RAM/core to reserve
+#SBATCH --time=26:00:00      #Maximum allocated time
+#SBATCH --output=myrun.out%j   #Path and name to the file for the STDOUT
+#SBATCH --error=myrun.err%j    #Path and name to the file for the STDERR
+                 #Load required modules
+#%Module torch
+source ~/pyVirtualEnv/bin/activate
+python DDPG_training.py    #Execute your command(s)
